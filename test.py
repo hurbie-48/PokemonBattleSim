a3678da357af424ee1f17a929b5cb2d872aa2d51
hurbie-48 <1136261@hr.nl>
2026-03-10 14:17:04 +0100

    Update Mango
    De mango is zoet en sappig

--- test.py
+++ test.py
@@ -3,5 +3,10 @@ from unittest.mock import patch
 
 clearScreen()
 showAscii()
+if not jsonExists():
+    name = askName(f"What is your name?: ")
+    player_id = addNewPlayer(name)
+    givePokemonToPlayer(player_id, 6)
+    save_to_json()
 showTrainers()
 askTrainer("Choose a trainer to battle against (type the name): ")
