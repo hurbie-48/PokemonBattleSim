fa148fef736214e6304c8959bd91a5878018f891
hurbie-48 <1136261@hr.nl>
2026-03-12 10:23:32 +0100

    Trainer Class

--- Player.py
+++ Player.py
@@ -2,9 +2,10 @@ from Pokemon import Pokemon
 
 
 class Player:
-    def __init__(self, name:str, pokemon:list[Pokemon]):
+    def __init__(self, name:str, pokemon:list[Pokemon], money:int):
         self.name = name
+        self.money = money
         self.pokemon = pokemon
 
     def __repr__(self):
-        return f"{self.name})"
+        return f"{self.name}"
