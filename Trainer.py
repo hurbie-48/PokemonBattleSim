36b3112d5e8dc76dccf86ad122de8e0e56fdb1a3
hurbie-48 <1136261@hr.nl>
2026-03-17 09:37:53 +0100

    Update Trainer.py

--- Trainer.py
+++ Trainer.py
@@ -7,14 +7,12 @@ class Trainer:
         self.is_beaten = False
 
     def get_active_pokemon(self):
-        """Returns the first pokemon that isn't fainted."""
         for p in self.pokemon:
             if p.is_alive():
                 return p
         return None
 
     def has_usable_pokemon(self) -> bool:
-        """Returns True if at least one pokemon has HP > 0."""
         return any(p.is_alive() for p in self.pokemon)
 
 
